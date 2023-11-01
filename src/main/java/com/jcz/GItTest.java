780f00a38887754d4cf79abbcfe6df477a03941d
jiangcuizhi <jiangcuizhi@jcz.com>
2023-11-01 20:36:35 +0800

    master commit2

--- src/main/java/com/jcz/GItTest.java
+++ src/main/java/com/jcz/GItTest.java
@@ -5,5 +5,6 @@ public class GItTest {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!");
         System.out.println("Hello hot-fix");
+        System.out.println("合并冲突分支master");
     }
 }
