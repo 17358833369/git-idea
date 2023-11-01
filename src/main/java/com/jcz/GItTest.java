d10361c6f0712b0d49843d7dad4feb5568006d78
jiangcuizhi <jiangcuizhi@jcz.com>
2023-11-01 21:10:25 +0800

    push commit

--- src/main/java/com/jcz/GItTest.java
+++ src/main/java/com/jcz/GItTest.java
@@ -7,5 +7,6 @@ public class GItTest {
         System.out.println("Hello hot-fix");
         System.out.println("合并冲突分支master");
         System.out.println("合并冲突分支hot-fix");
+        System.out.println("push test");
     }
 }
